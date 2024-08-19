64407c6e5108b005427026796e1c6bf3a6dda561
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-08-19 20:42:58 +0200

    Add bytes/packets received/transmitted metrics

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 class HyperswarmStats {
   constructor (swarm) {
     this.swarm = swarm
+
+    this._bytesTransmittedOverClosedSwarmStreams = 0
+    this._packetsTransmittedOverClosedSwarmStreams = 0
+    this._bytesReceivedOverClosedSwarmStreams = 0
+    this._packetsReceivedOverClosedSwarmStreams = 0
+    swarm.on('connection', conn => {
+      conn.on('close', () => {
+        this._bytesTransmittedOverClosedSwarmStreams += conn.rawStream?.bytesTransmitted || 0
+        this._packetsTransmittedOverClosedSwarmStreams += conn.rawStream?.packetsTransmitted || 0
+        this._bytesReceivedOverClosedSwarmStreams += conn.rawStream?.bytesReceived || 0
+        this._packetsReceivedOverClosedSwarmStreams += conn.rawStream?.packetsReceived || 0
+      })
+    })
   }
 
   get connects () {
@@ -17,6 +30,90 @@ class HyperswarmStats {
 
   get dhtQueries () {
     return this.swarm.dht.stats.queries
+  }
+
+  getBytesTransmittedAcrossAllStreams () {
+    let bytesFromCurrentConns = 0
+    for (const conn of this.swarm.connections) {
+      bytesFromCurrentConns += conn.rawStream?.bytesTransmitted || 0
+    }
+
+    return bytesFromCurrentConns + this._bytesTransmittedOverClosedSwarmStreams
+  }
+
+  getBytesReceivedAcrossAllStreams () {
+    let bytesFromCurrentConns = 0
+    for (const conn of this.swarm.connections) {
+      bytesFromCurrentConns += conn.rawStream?.bytesReceived || 0
+    }
+
+    return bytesFromCurrentConns + this._bytesReceivedOverClosedSwarmStreams
+  }
+
+  getPacketsTransmittedAcrossAllStreams () {
+    let packetsFromCurrentConns = 0
+    for (const conn of this.swarm.connections) {
+      packetsFromCurrentConns += conn.rawStream?.packetsTransmitted || 0
+    }
+
+    return packetsFromCurrentConns + this._packetsTransmittedOverClosedSwarmStreams
+  }
+
+  getPacketsReceivedAcrossAllStreams () {
+    let packetsFromCurrentConns = 0
+    for (const conn of this.swarm.connections) {
+      packetsFromCurrentConns += conn.rawStream?.packetsReceived || 0
+    }
+
+    return packetsFromCurrentConns + this._packetsReceivedOverClosedSwarmStreams
+  }
+
+  get dhtClientSocketBytesTransmitted () {
+    return this.swarm.dht.io.clientSocket?.bytesTransmitted || 0
+  }
+
+  get dhtClientSocketPacketsTransmitted () {
+    return this.swarm.dht.io.clientSocket?.packetsTransmitted || 0
+  }
+
+  get dhtClientSocketBytesReceived () {
+    return this.swarm.dht.io.clientSocket?.bytesReceived || 0
+  }
+
+  get dhtClientSocketPacketsReceived () {
+    return this.swarm.dht.io.clientSocket?.packetsReceived || 0
+  }
+
+  get dhtServerSocketBytesTransmitted () {
+    return this.swarm.dht.io.serverSocket?.bytesTransmitted || 0
+  }
+
+  get dhtServerSocketPacketsTransmitted () {
+    return this.swarm.dht.io.serverSocket?.packetsTransmitted || 0
+  }
+
+  get dhtServerSocketBytesReceived () {
+    return this.swarm.dht.io.serverSocket?.bytesReceived || 0
+  }
+
+  get dhtServerSocketPacketsReceived () {
+    return this.swarm.dht.io.serverSocket?.packetsReceived || 0
+  }
+
+  get udxBytesTransmitted () {
+    return this.swarm.dht.udx.bytesTransmitted
+  }
+
+  get udxPacketsTransmitted () {
+    return this.swarm.dht.udx.packetsTransmitted
+  }
+
+  get udxBytesReceived () {
+    return this.swarm.dht.udx.bytesReceived
+  }
+
+  get udxPacketsReceived () {
+    return this.swarm.dht.udx.packetsReceived
   }
 
   // TODO
@@ -108,6 +205,122 @@ class HyperswarmStats {
       help: 'Total number of queries in the dht-rpc instance',
       collect () {
         this.set(self.dhtQueries.total)
+      }
+    })
+
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'hyperswarm_total_bytes_transmitted',
+      help: 'todo',
+      collect () {
+        this.set(self.udxBytesTransmitted)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'hyperswarm_total_packets_transmitted',
+      help: 'todo',
+      collect () {
+        this.set(self.udxPacketsTransmitted)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'hyperswarm_total_bytes_received',
+      help: 'todo',
+      collect () {
+        this.set(self.udxBytesReceived)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'hyperswarm_total_packets_received',
+      help: 'todo',
+      collect () {
+        this.set(self.udxPacketsReceived)
+      }
+    })
+
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'dht_client_socket_bytes_transmitted',
+      help: 'todo',
+      collect () {
+        this.set(self.dhtClientSocketBytesTransmitted)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'dht_client_socket_packets_transmitted',
+      help: 'todo',
+      collect () {
+        this.set(self.dhtClientSocketPacketsTransmitted)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'dht_client_socket_bytes_received',
+      help: 'todo',
+      collect () {
+        this.set(self.dhtClientSocketBytesReceived)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'dht_client_socket_packets_received',
+      help: 'todo',
+      collect () {
+        this.set(self.dhtClientSocketPacketsReceived)
+      }
+    })
+
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'dht_server_socket_bytes_transmitted',
+      help: 'todo',
+      collect () {
+        this.set(self.dhtServerSocketBytesTransmitted)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'dht_server_socket_packets_transmitted',
+      help: 'todo',
+      collect () {
+        this.set(self.dhtClientSocketPacketsTransmitted)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'dht_server_socket_bytes_received',
+      help: 'todo',
+      collect () {
+        this.set(self.dhtServerSocketBytesReceived)
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'dht_server_socket_packets_received',
+      help: 'todo',
+      collect () {
+        this.set(self.dhtServerSocketPacketsReceived)
+      }
+    })
+
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'hyperswarm_total_bytes_transmitted_over_swarm_streams',
+      help: 'todo',
+      collect () {
+        this.set(self.getBytesTransmittedAcrossAllStreams())
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'hyperswarm_total_bytes_received_over_swarm_streams',
+      help: 'todo',
+      collect () {
+        this.set(self.getBytesReceivedAcrossAllStreams())
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'hyperswarm_total_packets_transmitted_over_swarm_streams',
+      help: 'todo',
+      collect () {
+        this.set(self.getPacketsTransmittedAcrossAllStreams())
+      }
+    })
+    new promClient.Gauge({ // eslint-disable-line no-new
+      name: 'hyperswarm_total_packets_received_over_swarm_streams',
+      help: 'todo',
+      collect () {
+        this.set(self.getPacketsReceivedAcrossAllStreams())
       }
     })
   }
